d1160bfd01c6d06f85c363ee4e3b99f4cbe1d83a
Michael Lesniak <mlesniak@micromata.de>
2017-04-23 16:57:23 +0200

    Add spring boot boilerplate code

--- src/main/java/com/micromata/webengineering/demo/Main.java
+++ src/main/java/com/micromata/webengineering/demo/Main.java
@@ -1,7 +1,11 @@
 package com.micromata.webengineering.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+        SpringApplication.run(Main.class, args);
     }
 }
