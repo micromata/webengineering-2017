f5d9f5204137a822c45d49ff5bea4ef27c4ed00c
Michael Lesniak <mlesniak@micromata.de>
2017-04-23 16:59:43 +0200

    Add simple endpoint to /

--- src/main/java/com/micromata/webengineering/demo/Main.java
+++ src/main/java/com/micromata/webengineering/demo/Main.java
@@ -2,10 +2,20 @@ package com.micromata.webengineering.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
+@RestController
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @RequestMapping("/")
+    public String index() {
+        return new Date().toString();
     }
 }
