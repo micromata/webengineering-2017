eaa4092ac1fc411463f90493c4ba37163ddf2a74
Michael Lesniak <mlesniak@micromata.de>
2017-04-23 17:50:16 +0200

    Fix swagger version

--- src/main/java/com/micromata/webengineering/demo/Main.java
+++ src/main/java/com/micromata/webengineering/demo/Main.java
@@ -2,13 +2,35 @@ package com.micromata.webengineering.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Main entry point and configuration base of the application.
  */
+@EnableSwagger2
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+
+    /**
+     * Configuration for Swagger.
+     *
+     * @return a docket.
+     */
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 }
