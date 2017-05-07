d353130143bbac93bc0cea18416acad5579afec6
Michael Lesniak <mlesniak@micromata.de>
2017-05-07 21:13:44 +0200

    Annotate Post as a JPA entity

--- src/main/java/com/micromata/webengineering/demo/post/Post.java
+++ src/main/java/com/micromata/webengineering/demo/post/Post.java
@@ -1,20 +1,20 @@
 package com.micromata.webengineering.demo.post;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.Date;
-import java.util.concurrent.atomic.AtomicLong;
 
+@Entity
 public class Post {
-    // Until we use actual data identity management over spring-data, we simulate unique id generation using
-    // atomic integers. This ensures that multiple requests to create new posts will receive unique ids.
-    private static AtomicLong nextId = new AtomicLong();
-
+    @Id
+    @GeneratedValue
     private Long id;
 
     private String title;
     private Date createdAt;
 
     public Post() {
-        id = nextId.getAndIncrement();
         createdAt = new Date();
     }
 
