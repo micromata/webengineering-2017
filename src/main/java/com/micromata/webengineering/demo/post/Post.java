5709fa010b54c29d7f6375336f7e6e8c3dac25bc
Michael Lesniak <mlesniak@micromata.de>
2017-05-07 14:58:39 +0200

    Add unique id to each new post

--- src/main/java/com/micromata/webengineering/demo/post/Post.java
+++ src/main/java/com/micromata/webengineering/demo/post/Post.java
@@ -1,12 +1,20 @@
 package com.micromata.webengineering.demo.post;
 
 import java.util.Date;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Post {
+    // Until we use actual data identity management over spring-data, we simulate unique id generation using
+    // atomic integers. This ensures that multiple requests to create new posts will receive unique ids.
+    private static AtomicLong nextId = new AtomicLong();
+
+    private Long id;
+
     private String title;
     private Date createdAt;
 
     public Post() {
+        id = nextId.getAndIncrement();
         createdAt = new Date();
     }
 
@@ -20,5 +28,9 @@ public class Post {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public Long getId() {
+        return id;
     }
 }
