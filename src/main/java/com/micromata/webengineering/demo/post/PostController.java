e90c07fee38f62c14a19491a6b26efb1a480c766
Michael Lesniak <mlesniak@micromata.de>
2017-05-07 15:02:22 +0200

    Add functionality to retrieve a single post

--- src/main/java/com/micromata/webengineering/demo/post/PostController.java
+++ src/main/java/com/micromata/webengineering/demo/post/PostController.java
@@ -1,10 +1,7 @@
 package com.micromata.webengineering.demo.post;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +21,10 @@ public class PostController {
     @RequestMapping(value = "/post", method = RequestMethod.POST)
     public void addPost(@RequestBody Post post) {
         postService.addPost(post);
+    }
+
+    @RequestMapping(value = "/post/{id}", method = RequestMethod.GET)
+    public Post getPost(@PathVariable Long id) {
+        return postService.getPost(id);
     }
 }
--- src/main/java/com/micromata/webengineering/demo/post/PostService.java
+++ src/main/java/com/micromata/webengineering/demo/post/PostService.java
@@ -30,4 +30,22 @@ public class PostService {
     public void addPost(Post post) {
         posts.add(post);
     }
+
+    /**
+     * Retrieve a single post.
+     *
+     * @param id post id
+     * @return post with the id or null if no post is found
+     */
+    public Post getPost(Long id) {
+        // This is enough functionality until we use proper data persistence techniques.
+        // Do not use this approach in production!
+        for (Post post : posts) {
+            if (post.getId().equals(id)) {
+                return post;
+            }
+        }
+
+        return null;
+    }
 }
