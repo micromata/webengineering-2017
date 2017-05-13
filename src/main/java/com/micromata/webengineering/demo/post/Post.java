79e738bf38feecbe3df00df9164722ce76378720
Michael Lesniak <mlesniak@micromata.de>
2017-05-13 09:03:41 +0200

    Use @PrePersist for updating date-related fields

--- src/main/java/com/micromata/webengineering/demo/post/Post.java
+++ src/main/java/com/micromata/webengineering/demo/post/Post.java
@@ -3,6 +3,7 @@ package com.micromata.webengineering.demo.post;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
 import java.util.Date;
 
 @Entity
@@ -14,23 +15,35 @@ public class Post {
     private String title;
     private Date createdAt;
 
-    public Post() {
-        createdAt = new Date();
-    }
 
     public String getTitle() {
         return title;
     }
 
+
     public Date getCreatedAt() {
         return createdAt;
     }
+
 
     public void setTitle(String title) {
         this.title = title;
     }
 
+
     public Long getId() {
         return id;
+    }
+
+
+    /**
+     * This method is called before an entity is persisted in the database. This is in contrast to our previous
+     * approach where an object's createdAt depends on the date of its instantiation.
+     * <p>
+     * Information about @PrePersist where found by using the search terms "jpa annotations createdat".
+     */
+    @PrePersist
+    public void prePersist() {
+        createdAt = new Date();
     }
 }
