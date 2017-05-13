d4f674f109f79df90cb2ce0120291b80e57ab06a
Michael Lesniak <mlesniak@micromata.de>
2017-05-13 08:51:14 +0200

    Add AddressService which encapsulates determination of host and ip

---
+++ src/main/java/com/micromata/webengineering/demo/util/AddressService.java
@@ -0,0 +1,61 @@
+package com.micromata.webengineering.demo.util;
+
+import org.springframework.boot.context.embedded.EmbeddedServletContainerInitializedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Service;
+
+import java.net.InetAddress;
+
+/**
+ * Helper functions to retrieve the server's hostname, ip and port for the running application.
+ * <p>
+ * See https://stackoverflow.com/questions/29929896/how-to-get-local-server-host-and-port-in-spring-boot as found by
+ * a google search for "spring-boot get host and ip".
+ */
+@Service
+@Configuration
+public class AddressService implements ApplicationListener<EmbeddedServletContainerInitializedEvent> {
+    private int port;
+
+    /**
+     * Return the host address as an IP address.
+     *
+     * @return address
+     */
+    public String getHostAddress() {
+        return InetAddress.getLoopbackAddress().getHostAddress();
+    }
+
+
+    /**
+     * Return the host address as a DNS-resolvable name.
+     *
+     * @return address
+     */
+    public String getHostName() {
+        return InetAddress.getLoopbackAddress().getHostName();
+    }
+
+
+    /**
+     * This method is called when a particular event (noted in the interface) is executed. In our case, the event
+     * was the start of the embedded application container as statet in the generic parameter.
+     *
+     * @param event the occurred event
+     */
+    @Override
+    public void onApplicationEvent(EmbeddedServletContainerInitializedEvent event) {
+        port = event.getEmbeddedServletContainer().getPort();
+    }
+
+
+    /**
+     * Return the port of the application.
+     *
+     * @return port
+     */
+    public int getPort() {
+        return port;
+    }
+}
