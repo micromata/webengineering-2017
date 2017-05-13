c3f3b5ce40ccc7df5eefbca88a7217ad58cfc18e
Michael Lesniak <mlesniak@micromata.de>
2017-05-13 17:35:35 +0200

    Add user POJO

---
+++ src/main/java/com/micromata/webengineering/demo/user/User.java
@@ -0,0 +1,30 @@
+package com.micromata.webengineering.demo.user;
+
+public class User {
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
