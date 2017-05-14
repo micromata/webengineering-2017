4f912139b6b83412210fdb46e70b7398b3239ba9
Michael Lesniak <mlesniak@micromata.de>
2017-05-14 10:10:28 +0200

    Add JavaDoc

--- src/main/java/com/micromata/webengineering/demo/authentication/AuthenticationService.java
+++ src/main/java/com/micromata/webengineering/demo/authentication/AuthenticationService.java
@@ -16,12 +16,22 @@ public class AuthenticationService {
 
     private String secret = "Severus Snape was a good guy!";
 
+    /**
+     * Return object containing a valid user and his corresponding JWT token.
+     */
     public static class UserToken {
         public User user;
         public String token;
     }
 
 
+    /**
+     * Create a JWT token and additional user information if the user's credentails are valid.
+     *
+     * @param email    email
+     * @param password password
+     * @return a UserToken or null if the credentials are not valid
+     */
     public UserToken login(String email, String password) {
         User user = userService.getUser(email, password);
         if (user == null) {
@@ -41,6 +51,13 @@ public class AuthenticationService {
     }
 
 
+    /**
+     * Validate that a token is valid and returns its body.
+     *
+     * Throws a SignatureException if the token is not valid.
+     * @param jwtToken JWT token
+     * @return JWT body
+     */
     public Object parseToken(String jwtToken) {
         return Jwts.parser()
                 .setSigningKey(secret)
@@ -48,7 +65,12 @@ public class AuthenticationService {
                 .getBody();
     }
 
-
+    /**
+     * Set a user for the current request.
+     *
+     * @param id user id
+     * @param email user email
+     */
     public void setUser(Long id, String email) {
         User user = new User();
         user.setId(id);
