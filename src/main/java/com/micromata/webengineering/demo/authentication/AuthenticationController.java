6e9bd23d04e672492269a5984f18f33b0b4ab8e6
Michael Lesniak <mlesniak@micromata.de>
2017-05-14 08:16:47 +0200

    Validate credentails against database

--- src/main/java/com/micromata/webengineering/demo/authentication/AuthenticationController.java
+++ src/main/java/com/micromata/webengineering/demo/authentication/AuthenticationController.java
@@ -1,6 +1,8 @@
 package com.micromata.webengineering.demo.authentication;
 
 import com.micromata.webengineering.demo.user.User;
+import com.micromata.webengineering.demo.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,12 +22,16 @@ public class AuthenticationController {
     }
 
 
+    @Autowired
+    private UserRepository userRepository;
+
     @RequestMapping(value = "login", method = RequestMethod.POST)
     public UserToken login(@RequestBody UserLogin userLogin) {
+        // Test, that retrieval of user works.
+        User user = userRepository.login(userLogin.email, userLogin.password);
+
         UserToken token = new UserToken();
-        token.user = new User();
-        token.user.setEmail(userLogin.email);
-        token.user.setId(1L);
+        token.user = user;
         token.token = "<JWT-TOKEN>";
 
         return token;
--- src/main/java/com/micromata/webengineering/demo/user/UserRepository.java
+++ src/main/java/com/micromata/webengineering/demo/user/UserRepository.java
@@ -7,4 +7,7 @@ import org.springframework.data.repository.query.Param;
 public interface UserRepository extends CrudRepository<User, Long> {
     @Query("SELECT u FROM User_ u WHERE u.email = :email")
     User findByEmail(@Param("email") String email);
+
+    @Query("SELECT u from User_ u WHERE u.email = :email AND u.password = :password")
+    User login(@Param("email") String email, @Param("password") String password);
 }
