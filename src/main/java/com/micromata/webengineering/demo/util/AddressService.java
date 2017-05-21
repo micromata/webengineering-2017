ea1998c5887e71421f6da7e230c63a250a7898ad
Michael Lesniak <mlesniak@micromata.de>
2017-05-21 07:41:16 +0200

    Return correct server url depending on the deployment environment

--- src/main/java/com/micromata/webengineering/demo/util/AddressService.java
+++ src/main/java/com/micromata/webengineering/demo/util/AddressService.java
@@ -1,70 +1,25 @@
 package com.micromata.webengineering.demo.util;
 
-import org.springframework.boot.context.embedded.EmbeddedServletContainerInitializedEvent;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.net.InetAddress;
-
 /**
- * Helper functions to retrieve the server's hostname, ip and port for the running application.
- * <p>
- * See https://stackoverflow.com/questions/29929896/how-to-get-local-server-host-and-port-in-spring-boot as found by
- * a google search for "spring-boot get host and ip".
+ * Helper functions to retrieve server URL for return correct entity resource paths.
+ *
+ * Since we possibly are behind load balancers and want to be as deployment agnostic as possible we simply configure
+ * our host URL.
  */
 @Service
-@Configuration
-public class AddressService implements ApplicationListener<EmbeddedServletContainerInitializedEvent> {
-    private int port;
+public class AddressService {
+    @Value("${addressService.address}")
+    private String serverAddress;
 
     /**
-     * Return the host address as an IP address.
-     *
-     * @return address
-     */
-    public String getHostAddress() {
-        return InetAddress.getLoopbackAddress().getHostAddress();
-    }
-
-
-    /**
-     * Return the host address as a DNS-resolvable name.
-     *
-     * @return address
-     */
-    public String getHostName() {
-        return InetAddress.getLoopbackAddress().getHostName();
-    }
-
-
-    /**
-     * This method is called when a particular event (noted in the interface) is executed. In our case, the event
-     * was the start of the embedded application container as statet in the generic parameter.
-     *
-     * @param event the occurred event
-     */
-    @Override
-    public void onApplicationEvent(EmbeddedServletContainerInitializedEvent event) {
-        port = event.getEmbeddedServletContainer().getPort();
-    }
-
-
-    /**
-     * Return the port of the application.
-     *
-     * @return port
-     */
-    public int getPort() {
-        return port;
-    }
-
-    /**
-     * Return server URL with http:// prefix.
+     * Return server URL.
      *
      * @return server URL.
      */
     public String getServerURL() {
-        return "http://" + getHostName() + ":" + getPort();
+        return serverAddress;
     }
 }
