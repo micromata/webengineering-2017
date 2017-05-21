6a1d55ae8417b859fee86599a728e957cdfb8439
Michael Lesniak <mlesniak@micromata.de>
2017-05-21 07:54:57 +0200

    Update credentials with hashes using salt

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -9,8 +9,8 @@ DELETE FROM USER_;
 
 -- Insert new users.
 INSERT INTO USER_ (id, email, PASSWORD) VALUES
-  (1, 'michael', 'f7fbba6e0636f890e56fbbf3283e524c6fa3204ae298382d624741d0dc6638326e282c41be5e4254d8820772c5518a2c5a8c0c7f7eda19594a7eb539453e1ed7'), -- foo
-  (2, 'kai', 'd82c4eb5261cb9c8aa9855edd67d1bd10482f41529858d925094d173fa662aa91ff39bc5b188615273484021dfb16fd8284cf684ccf0fc795be3aa2fc1e6c181');     -- bar
+  (1, 'michael', 'c5ce3a9cb904b362072cabbdaeef90d5bd73680a478b509ebf1d013b4ad77b92e7706114a5bbbf160715396606e01fbfb2cc4ef5bffef4cfd0dea74ca04f2f5f'), -- foo
+  (2, 'kai',     '9036c1111fdcbfa01a60a3f5d4647e7369caf5153b5e65eb7f45688748d070157fe9b62650ee564fbbcb42e6efc79df3238b8e1c10325b2e998f6d882e4d2071');     -- bar
 
 -- Add some posts.
 INSERT INTO POST (id, title, AUTHOR_ID) VALUES
