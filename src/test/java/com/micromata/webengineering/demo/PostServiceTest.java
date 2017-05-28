210c2173b5d15591cdfccb0999ac11eb707659b5
Michael Lesniak <mlesniak@micromata.de>
2017-05-28 06:42:28 +0200

    Check that Dependency Injection works

--- src/test/java/com/micromata/webengineering/demo/PostServiceTest.java
+++ src/test/java/com/micromata/webengineering/demo/PostServiceTest.java
@@ -1,13 +1,18 @@
 package com.micromata.webengineering.demo;
 
 
+import com.micromata.webengineering.demo.post.PostService;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
 
 public class PostServiceTest {
+    @Autowired
+    private PostService postService;
+
     @Test
-    public void sunshine() {
-        assertTrue(true);
+    public void notNull() {
+        assertNotNull("We should have an instance of postService", postService);
     }
 }
