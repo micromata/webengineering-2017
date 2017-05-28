096ee525cdc7c4e05d12116c0b354edeee581ed0
Michael Lesniak <mlesniak@micromata.de>
2017-05-28 08:04:26 +0200

    Add additional service implementation for tests

---
+++ src/test/java/com/micromata/webengineering/demo/post/util/AddressServiceTestImpl.java
@@ -0,0 +1,17 @@
+package com.micromata.webengineering.demo.post.util;
+
+
+import com.micromata.webengineering.demo.util.AddressService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Note that this is a CONSTRUCTED example since in this particular case we would simple change the definition
+ * in application.properties.
+ */
+@Service
+public class AddressServiceTestImpl implements AddressService {
+    @Override
+    public String getServerURL() {
+        return "foo";
+    }
+}
