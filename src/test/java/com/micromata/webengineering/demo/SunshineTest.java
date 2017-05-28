d03b2cedca971ebc71e20abd67af5368c1d960fe
Michael Lesniak <mlesniak@micromata.de>
2017-05-28 06:29:54 +0200

    Add sunshine test
    Check that everything works

---
+++ src/test/java/com/micromata/webengineering/demo/SunshineTest.java
@@ -0,0 +1,13 @@
+package com.micromata.webengineering.demo;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class SunshineTest {
+    @Test
+    public void sunshine() {
+        assertTrue(true);
+    }
+}
