b1f49e7e8dfc2c309c4a56a0b252f449fdeb66d8
Michael Lesniak <mlesniak@micromata.de>
2017-05-28 07:44:56 +0200

    Test that storing a post actually works

--- src/test/java/com/micromata/webengineering/demo/PostServiceTest.java
+++ src/test/java/com/micromata/webengineering/demo/PostServiceTest.java
@@ -11,7 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.assertNotNull;
+import java.util.UUID;
+
+import static org.junit.Assert.*;
 
 // Use Spring's testing support in JUnit
 @RunWith(SpringRunner.class)
@@ -29,15 +31,49 @@ public class PostServiceTest {
         userService.setCurrentUser(1L, "michael");
     }
 
+    /**
+     * Test that dependency injection works.
+     */
     @Test
     public void notNull() {
         assertNotNull("We should have an instance of postService", postService);
     }
 
+
+    /**
+     * Test that adding a new post leads to an id (and the post is thus persisted).
+     */
     @Test
     public void testPostAdd() {
         Post post = new Post();
         post.setTitle("Test");
+
+        assertNull(post.getId());
         postService.addPost(post);
+        assertNotNull(post.getId());
+    }
+
+
+    /**
+     * Test that a post is actually persisted.
+     * <p>
+     * NOTE: Usually we would actually test business logic which is implemented in the service. In our case there is
+     * de facto no business logic hence we simply test a few persistence features, but keep in mind what I said about
+     * testing core technologies in the lecture.
+     */
+    @Test
+    public void testPostPersisted() {
+        String uuid = UUID.randomUUID().toString();
+
+        Post post = new Post();
+        post.setTitle(uuid);
+
+        assertNull(post.getId());
+        postService.addPost(post);
+        assertNotNull(post.getId());
+        Long id = post.getId();
+
+        Post storedPost = postService.getPost(id);
+        assertEquals("Post correctly stored", storedPost.getTitle(), uuid);
     }
 }
