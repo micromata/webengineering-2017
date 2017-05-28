4b8603115591d7bc355f7bce7308290cbf76c522
Michael Lesniak <mlesniak@micromata.de>
2017-05-28 08:29:26 +0200

    Add request with correct authentication and validation of response

--- src/test/java/com/micromata/webengineering/demo/integration/IntegrationTest.java
+++ src/test/java/com/micromata/webengineering/demo/integration/IntegrationTest.java
@@ -8,6 +8,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
@@ -47,16 +49,23 @@ public class IntegrationTest {
     @Test
     public void testAddPost() {
         RestTemplate rest = new RestTemplate();
-
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJrYWkiLCJqdGkiOiIyIn0.h3ezvhsje3tpvHbXxz7TUmy7KhT5yjtljXKvDeo8MM2RTAEIP6l2vdRHw2KKg0-HgK-8CsMY5im3kp6zIogUTQ");
         Post post = new Post();
-        post.setTitle("test-post");
-        ResponseEntity<Map> response = rest.postForEntity(getPostURL(), post, Map.class);
+        String title = "test-post";
+        post.setTitle(title);
+        HttpEntity<Post> entity = new HttpEntity<>(post, headers);
+        ResponseEntity<Map> response = rest.postForEntity(getPostURL(), entity, Map.class);
+
+
+        String url = (String) response.getBody().get("url");
+        Post storedPost = rest.getForObject(url, Post.class);
+        assertEquals(title, storedPost.getTitle());
+        assertEquals("kai", storedPost.getAuthor().getEmail());
     }
 
 
     private String getPostURL() {
         return "http://localhost:" + port + "/api/post";
     }
-
-
 }
