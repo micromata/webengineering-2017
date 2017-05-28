1415ad86fc7d0d30cbf79b698d4d2e3a58208fd2
Michael Lesniak <mlesniak@micromata.de>
2017-05-28 06:40:37 +0200

    Rename test class

--- src/test/java/com/micromata/webengineering/demo/PostServiceTest.java
+++ src/test/java/com/micromata/webengineering/demo/PostServiceTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
-public class SunshineTest {
+public class PostServiceTest {
     @Test
     public void sunshine() {
         assertTrue(true);
