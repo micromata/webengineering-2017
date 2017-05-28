edb8694afeaec8cd81ee416403acf9bd674e76a9
Michael Lesniak <mlesniak@micromata.de>
2017-05-28 06:45:11 +0200

    Enable spring support for unit testing

--- src/test/java/com/micromata/webengineering/demo/PostServiceTest.java
+++ src/test/java/com/micromata/webengineering/demo/PostServiceTest.java
@@ -3,10 +3,17 @@ package com.micromata.webengineering.demo;
 
 import com.micromata.webengineering.demo.post.PostService;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertNotNull;
 
+// Use Spring's testing support in JUnit
+@RunWith(SpringRunner.class)
+// Enable Spring features, e.g. loading of application-properties, etc.
+@SpringBootTest
 public class PostServiceTest {
     @Autowired
     private PostService postService;
