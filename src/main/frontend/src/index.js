6ceb761b45fca9f875bd661070ce8f107ddb4ec4
Michael Lesniak <mlesniak@micromata.de>
2017-06-15 10:40:43 +0200

    Create a new post (NOT WORKING)

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -1,13 +1,18 @@
 import axios from "axios";
 import React from "react";
 
+// Create a new post with a random title.
+axios.post('/api/post', {
+    title: 'axios-' + new Date().getTime()
+});
+
 // Retrieve list of posts and show them.
-axios.get('/api/post')
-    .then(({data}) => {
-        for (var post of data) {
-            console.log(post);
-        }
-    });
+// axios.get('/api/post')
+//     .then(({data}) => {
+//         for (var post of data) {
+//             console.log(post);
+//         }
+//     });
 
 
 // This annoys me while looking at the browser ;-)
