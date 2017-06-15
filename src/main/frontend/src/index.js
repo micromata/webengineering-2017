24f57c81e5a58559c3144d8c3b64157f52599ef2
Michael Lesniak <mlesniak@micromata.de>
2017-06-15 10:46:05 +0200

    Create a new post with authentication using JWT

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -2,9 +2,16 @@ import axios from "axios";
 import React from "react";
 
 // Create a new post with a random title.
-axios.post('/api/post', {
-    title: 'axios-' + new Date().getTime()
-});
+axios.post('/api/post',
+    {
+        // POST data
+        title: 'axios-' + new Date().getTime()
+    }, {
+        // Configuration
+        headers: {
+            Authorization: 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJrYWkiLCJqdGkiOiIyIn0.h3ezvhsje3tpvHbXxz7TUmy7KhT5yjtljXKvDeo8MM2RTAEIP6l2vdRHw2KKg0-HgK-8CsMY5im3kp6zIogUTQ'
+        }
+    });
 
 // Retrieve list of posts and show them.
 // axios.get('/api/post')
