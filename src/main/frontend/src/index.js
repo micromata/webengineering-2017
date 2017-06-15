d239d82051aff4b800e009a80f2c2eafd86a65dd
Michael Lesniak <mlesniak@micromata.de>
2017-06-15 10:20:42 +0200

    Cleanup axios playground

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -1,18 +1,8 @@
-import axios from "axios";
 import React from "react";
 import ReactDOM from "react-dom";
 
 import Greeter from "./components/greeter";
 
-
-// Let' write some axios test code and check that everything works with console.log. This is
-// comfortable and we do not need to modify our react code to check that we have understood how
-// axios works.
-
-axios.get('http://www.mlesniak.com')
-    .then(({data}) =>
-        console.log(data)
-    );
 
 ReactDOM.render(
     <div>
