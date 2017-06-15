31f848aa1c431c32041201375eeb28be555e6b6a
Michael Lesniak <mlesniak@micromata.de>
2017-06-15 09:49:23 +0200

    Show Greeter as a functional component

--- src/main/frontend/src/components/greeter.js
+++ src/main/frontend/src/components/greeter.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Counter from "./counter";
 
-class Greeter extends React.Component {
-    render() {
-        const message = !this.props.message ? 'unknown' : this.props.message;
-        return <h1>Hello, {message}! (<Counter />)</h1>
-    }
-}
+// class Greeter extends React.Component {
+//     render() {
+//         const message = !this.props.message ? 'unknown' : this.props.message;
+//         return <h1>Hello, {message}! (<Counter />)</h1>
+//     }
+// }
+
+
+const Greeter = (props) => {
+    const message = !props.message ? 'unknown' : props.message;
+    return <h1>Hello (functional component), {message}! (<Counter />)</h1>
+};
 
 export default Greeter;
