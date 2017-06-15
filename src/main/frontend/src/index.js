dba4af3bd87c9721e706b939f7ab1fb4fa6c80e9
Michael Lesniak <mlesniak@micromata.de>
2017-06-15 10:52:25 +0200

    Add global authentication to axios requests

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -1,16 +1,15 @@
 import axios from "axios";
 import React from "react";
 
+// Set authorization header globally for all requests.
+// See https://github.com/mzabriskie/axios#global-axios-defaults
+axios.defaults.headers.common['Authorization'] = 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJrYWkiLCJqdGkiOiIyIn0.h3ezvhsje3tpvHbXxz7TUmy7KhT5yjtljXKvDeo8MM2RTAEIP6l2vdRHw2KKg0-HgK-8CsMY5im3kp6zIogUTQ';
+
 // Create a new post with a random title.
 axios.post('/api/post',
     {
         // POST data
-        title: 'axios-' + new Date().getTime()
-    }, {
-        // Configuration
-        headers: {
-            Authorization: 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJrYWkiLCJqdGkiOiIyIn0.h3ezvhsje3tpvHbXxz7TUmy7KhT5yjtljXKvDeo8MM2RTAEIP6l2vdRHw2KKg0-HgK-8CsMY5im3kp6zIogUTQ'
-        }
+        title: 'with-global-auth // axios-' + new Date().getTime()
     });
 
 // Retrieve list of posts and show them.
