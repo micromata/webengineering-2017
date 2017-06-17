ac86959e9d3d79879ebdbab3fd3cdbea7ed7c55b
Michael Lesniak <mlesniak@micromata.de>
2017-06-17 18:42:32 +0200

    Add initial empty post component

---
+++ src/main/frontend/src/components/post_list.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+class PostList extends React.Component {
+    render() {
+        return <h1>Posts</h1>
+    }
+}
+
+
+export default PostList;
--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -1,33 +1,33 @@
 import axios from "axios";
 import React from "react";
+import ReactDOM from "react-dom";
+
+import PostList from "./components/post_list";
 
 // Set authorization header globally for all requests.
 // See https://github.com/mzabriskie/axios#global-axios-defaults
+//
 axios.defaults.headers.common['Authorization'] = 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJrYWkiLCJqdGkiOiIyIn0.h3ezvhsje3tpvHbXxz7TUmy7KhT5yjtljXKvDeo8MM2RTAEIP6l2vdRHw2KKg0-HgK-8CsMY5im3kp6zIogUTQ';
 
 // Create a new post with a random title.
-axios.post('/api/post',
-    {
-        // POST data
-        title: 'with-global-auth // axios-' + new Date().getTime()
-    });
-
-// Retrieve list of posts and show them.
-axios.get('/api/post')
-    .then(({data}) => {
-        for (var post of data) {
-            console.log(post);
-        }
-    });
-
-
-// This annoys me while looking at the browser ;-)
+// axios.post('/api/post',
+//     {
+//         // POST data
+//         title: 'with-global-auth // axios-' + new Date().getTime()
+//     });
 //
-// ReactDOM.render(
-//     <div>
-//         <Greeter message="Students"/>
-//         <Greeter message="Michael"/>
-//         <Greeter />
-//     </div>,
-//     document.getElementById('root'));
+// // Retrieve list of posts and show them.
+// axios.get('/api/post')
+//     .then(({data}) => {
+//         for (var post of data) {
+//             console.log(post);
+//         }
+//     });
+
+
+ReactDOM.render(
+    <div>
+        <PostList />
+    </div>,
+    document.getElementById('root'));
 
