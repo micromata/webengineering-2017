aec5d0d2c91c0973172b7ea6ef6fe3b7b03d98c8
Michael Lesniak <mlesniak@micromata.de>
2017-06-17 19:47:35 +0200

    Render list of example posts (NOT WORKING)

--- src/main/frontend/src/components/post_list.js
+++ src/main/frontend/src/components/post_list.js
@@ -2,8 +2,32 @@ import React from "react";
 
 
 class PostList extends React.Component {
+    constructor(props) {
+        super();
+        this.state = {
+            posts: []
+        }
+    }
+
+    // This function is called before render() to initialize its state.
+    componentWillMount() {
+        this.setState({
+            posts: [
+                {
+                    title: 'test -- loading will be implemented later'
+                }
+            ]
+        });
+    }
+
+
     render() {
-        return <h1>Posts</h1>
+        return (
+            <div>
+                <h1>Posts</h1>
+                {this.state.posts}
+            </div>
+        );
     }
 }
 
