23f1d4ab94695206cbd1e9b9f3a6ce39d4963dad
Michael Lesniak <mlesniak@micromata.de>
2017-06-18 10:55:06 +0200

    Add anonymous function to prevent calling of delete while rendering

--- src/main/frontend/src/components/post_list.js
+++ src/main/frontend/src/components/post_list.js
@@ -27,7 +27,7 @@ class PostList extends React.Component {
         return this.state.posts.map((post => {
             return (
                 <li key={post.id}>
-                    {post.id} {post.title} <span onClick={this.deletePost(post.id)}>DELETE</span>
+                    {post.id} {post.title} <span onClick={() => this.deletePost(post.id)}>DELETE</span>
                 </li>
             );
         }));
