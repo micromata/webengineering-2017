7f8569b0950e56aeec725962e92e5f29f84b15fe
Michael Lesniak <mlesniak@micromata.de>
2017-06-18 09:39:52 +0200

    Cleanup

--- src/main/frontend/src/components/post_create.js
+++ src/main/frontend/src/components/post_create.js
@@ -8,28 +8,12 @@ class PostCreate extends React.Component {
             title: 'default title'
         };
 
-        // This is easier than adding it in the JSX part and you don't create a new function every time your component
-        // is rendered.
-        //
-        // See https://medium.freecodecamp.com/react-binding-patterns-5-approaches-for-handling-this-92c651b5af56
-        //
-        // For general remarks regarding this in JavaScript see, e.g.
-        // - http://developer.telerik.com/featured/seven-javascript-quirks-i-wish-id-known-about/
-        // - http://ryanmorr.com/understanding-scope-and-context-in-javascript/
-        //
-        // (This comment will be deleted in some of the future commits.)
         this.handleTitleChange = this.handleTitleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
 
     handleTitleChange(event) {
-        // Remember setState behaviour:
-        //
-        //     "You may optionally pass an object as the first argument to setState() instead of a function: [...]
-        //     This performs a shallow merge of stateChange into the new state [...]."
-        //
-        // See https://facebook.github.io/react/docs/react-component.html#setstate
         this.setState({title: event.target.value}, () => console.log(this.state.title));
     }
 
