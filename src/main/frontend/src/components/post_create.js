f9d4b08e357aeb047f995cba703fc730922ef2a6
Michael Lesniak <mlesniak@micromata.de>
2017-06-18 09:29:09 +0200

    Use component state to store title

--- src/main/frontend/src/components/post_create.js
+++ src/main/frontend/src/components/post_create.js
@@ -2,13 +2,21 @@ import React from "react";
 
 // See https://facebook.github.io/react/docs/forms.html for documentation about forms.
 class PostCreate extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: 'default title'
+        };
+    }
+
+
     render() {
         return (
             <div>
                 <form>
                     <label>
                         title
-                        <input type="text" name="title"/>
+                        <input type="text" name="title" value={this.state.title}/>
                     </label>
                     <input type="submit" value="Submit"/>
                 </form>
