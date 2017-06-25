7b56638a67d3e37767d7157463e7e8d1f35a89fa
Michael Lesniak <mlesniak@micromata.de>
2017-06-25 06:05:47 +0200

    Add state and event handling for email and password

--- src/main/frontend/src/components/authentication.js
+++ src/main/frontend/src/components/authentication.js
@@ -1,19 +1,47 @@
 import React from "react";
 
 class Authentication extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        };
+
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+
+    handleEmailChange(event) {
+        this.setState({email: event.target.value});
+    }
+
+
+    handlePasswordChange(event) {
+        this.setState({password: event.target.value});
+    }
+
+
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log(this.state);
+    }
+
     render() {
         return (
             <div className="component">
                 <h1>Authentication</h1>
-                <form>
-                    {/*Note that the HTML structure will be changed later when we add Bootstrap/CSS*/}
+                <form onSubmit={this.handleSubmit}>
                     <label>
                         Email
-                        <input type="text" name="email"/>
+                        <input type="text" name="email" value={this.state.email} onChange={this.handleEmailChange}/>
                     </label>
                     <label>
                         Password
-                        <input type="password" name="password"/>
+                        <input type="password" name="password" value={this.state.password}
+                               onChange={this.handlePasswordChange}/>
                     </label>
                     <input type="submit" value="Submit"/>
                 </form>
