e5053d4da4cb7f6596376b42f273614e453995f8
Michael Lesniak <mlesniak@micromata.de>
2017-06-25 05:57:21 +0200

    Add simple HTML for email and password

--- src/main/frontend/src/components/authentication.js
+++ src/main/frontend/src/components/authentication.js
@@ -5,6 +5,18 @@ class Authentication extends React.Component {
         return (
             <div className="component">
                 <h1>Authentication</h1>
+                <form>
+                    {/*Note that the HTML structure will be changed later when we add Bootstrap/CSS*/}
+                    <label>
+                        Email
+                        <input type="text" name="email"/>
+                    </label>
+                    <label>
+                        Password
+                        <input type="password" name="password"/>
+                    </label>
+                    <input type="submit" value="Submit"/>
+                </form>
             </div>
         );
     }
