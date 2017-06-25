6dd3c3b8a5e7f67119f0891fe3e940c2cb7358f4
Michael Lesniak <mlesniak@micromata.de>
2017-06-25 13:37:11 +0200

    Show actual post details

--- src/main/frontend/src/components/post_detail.js
+++ src/main/frontend/src/components/post_detail.js
@@ -1,11 +1,39 @@
+import axios from "axios";
 import React from "react";
 
 class PostDetail extends React.Component {
+    constructor(props) {
+        super();
+        this.state = {
+            post: undefined
+        }
+    }
+
+    componentWillMount() {
+        axios.get(`/api/post/${this.props.match.params.id}`)
+            .then(({data}) => {
+                this.setState({
+                    post: data
+                });
+            });
+    }
+
+
     render() {
+        const post = this.state.post;
+        if (!post) {
+            // Do not show anything while loading.
+            return <div></div>;
+        }
+
+        console.log(post);
+
         return (
             <div className="component">
                 <h1>Post Detail</h1>
-                {this.props.match.params.id}
+                <div>Title {post.title}</div>
+                <div>Author {post.author.email}</div>
+                <div>Created at {new Date(post.createdAt).toISOString()}</div>
             </div>
         );
     }
