ab5eaf0f5137dee38703fc7d8a962d0fec8758d4
Michael Lesniak <mlesniak@micromata.de>
2017-06-25 06:41:30 +0200

    Remove default authentication

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -1,15 +1,9 @@
-import axios from "axios";
 import React from "react";
 import ReactDOM from "react-dom";
 import Authentication from "./components/authentication";
 
 import PostCreate from "./components/post_create";
 import PostList from "./components/post_list";
-
-// Set authorization header globally for all requests.
-// See https://github.com/mzabriskie/axios#global-axios-defaults
-//
-axios.defaults.headers.common['Authorization'] = 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJrYWkiLCJqdGkiOiIyIn0.h3ezvhsje3tpvHbXxz7TUmy7KhT5yjtljXKvDeo8MM2RTAEIP6l2vdRHw2KKg0-HgK-8CsMY5im3kp6zIogUTQ';
 
 
 ReactDOM.render(
