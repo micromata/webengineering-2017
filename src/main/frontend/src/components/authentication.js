e13cd15272bf17a072f592e74acbd9a9aabbf66a
Michael Lesniak <mlesniak@micromata.de>
2017-06-25 07:14:33 +0200

    Store successful authentication in a cookie

--- src/main/frontend/src/components/authentication.js
+++ src/main/frontend/src/components/authentication.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 import React from "react";
+import {withCookies} from "react-cookie";
+
 import User from "../util/User";
 
 class Authentication extends React.Component {
@@ -15,7 +17,6 @@ class Authentication extends React.Component {
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-
     handleEmailChange(event) {
         this.setState({email: event.target.value});
     }
@@ -27,12 +28,26 @@ class Authentication extends React.Component {
 
 
     handleSubmit(event) {
+        // Syntactic ES6 sugar for
+        //
+        //   cookies.get = this.props.get
+        //   cookies.set = this.props.set
+        //   ..
+        //
+        const {cookies} = this.props;
+
         event.preventDefault();
         axios.post('/user/login', this.state)
             .then(({data}) => {
                 axios.defaults.headers.common['Authorization'] = `Bearer ${data.token}`;
                 User.email = data.user.email;
                 User.id = data.user.id;
+
+                // Store authentication values even after refresh.
+                cookies.set('auth', {
+                    token: data.token,
+                    user: User
+                }, {path: '/'});
 
                 // Since we do not have the User as part of the component's state,
                 // calling this.SetState() makes no sense. Instead we have to manually
@@ -66,4 +81,4 @@ class Authentication extends React.Component {
 }
 
 
-export default Authentication;
+export default withCookies(Authentication);
--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -1,16 +1,20 @@
 import React from "react";
+import {CookiesProvider} from "react-cookie";
 import ReactDOM from "react-dom";
-import Authentication from "./components/authentication";
 
+import Authentication from "./components/authentication";
 import PostCreate from "./components/post_create";
 import PostList from "./components/post_list";
 
 
 ReactDOM.render(
-    <div>
-        <PostList />
-        <PostCreate/>
-        <Authentication />
-    </div>,
+    // This component will insert a property cookies to each child.
+    <CookiesProvider>
+        <div>
+            <PostList />
+            <PostCreate/>
+            <Authentication />
+        </div>
+    </CookiesProvider>,
     document.getElementById('root'));
 
