8291f5b8da22d9f708a1dc4737c22b4b80b196b7
Michael Lesniak <mlesniak@micromata.de>
2017-06-25 05:51:08 +0200

    Add simple border around component for better identification

--- src/main/frontend/src/components/authentication.js
+++ src/main/frontend/src/components/authentication.js
@@ -3,7 +3,7 @@ import React from "react";
 class Authentication extends React.Component {
     render() {
         return (
-            <div>
+            <div style={{border: '1px solid lightgray'}}>
                 <h1>Authentication</h1>
             </div>
         );
