9e1d01b8b1a1b06e5d8cc22d5ab65ba9e718803e
Michael Lesniak <mlesniak@micromata.de>
2017-06-25 08:51:48 +0200

    Add links and simple menu bar

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {CookiesProvider} from "react-cookie";
 import ReactDOM from "react-dom";
-import {HashRouter as Router, Route, Switch} from "react-router-dom";
+import {HashRouter as Router, Link, Route, Switch} from "react-router-dom";
 
 
 import Authentication from "./components/authentication";
@@ -11,8 +11,13 @@ import PostList from "./components/post_list";
 
 ReactDOM.render(
     <CookiesProvider>
-        <div>
-            <Router>
+        <Router>
+            <div>
+                <div className="menu">
+                    <Link to="/post/new">Create post</Link>
+                    <Link to="/">Post list</Link>
+                    <Link to="/user/login">Login</Link>
+                </div>
                 <Switch>
                     {/*Authentication*/}
                     <Route path="/user/login" component={Authentication}/>
@@ -23,8 +28,8 @@ ReactDOM.render(
                     {/*Default route*/}
                     <Route path="/" component={PostList}/>
                 </Switch>
-            </Router>
-        </div>
+            </div>
+        </Router>
     </CookiesProvider>,
     document.getElementById('root'));
 
