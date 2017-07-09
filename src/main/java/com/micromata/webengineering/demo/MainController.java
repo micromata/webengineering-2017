3a31f712f4cca56b63259afd9e6e186a65df3afb
Michael Lesniak <mlesniak@micromata.de>
2017-07-09 09:02:51 +0200

    Add form processing

--- src/main/java/com/micromata/webengineering/demo/MainController.java
+++ src/main/java/com/micromata/webengineering/demo/MainController.java
@@ -2,7 +2,9 @@ package com.micromata.webengineering.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Date;
 
@@ -11,6 +13,12 @@ public class MainController {
     @RequestMapping("/")
     public String index(Model model) {
         model.addAttribute("value", new Date());
+        return "index";
+    }
+
+    @RequestMapping(value = "/form", method = RequestMethod.POST)
+    public String updateValue(@ModelAttribute("value") String value, Model model) {
+        model.addAttribute("value", value);
         return "index";
     }
 }
