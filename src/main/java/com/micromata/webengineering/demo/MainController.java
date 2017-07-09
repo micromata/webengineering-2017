f29110469a95cf9a6fbd4eaba3138bb8decf63d8
Michael Lesniak <mlesniak@micromata.de>
2017-07-09 08:33:37 +0200

    Remove automatic rendering of objects to JSON

--- src/main/java/com/micromata/webengineering/demo/MainController.java
+++ src/main/java/com/micromata/webengineering/demo/MainController.java
@@ -2,13 +2,11 @@ package com.micromata.webengineering.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Date;
 
 @Controller
 public class MainController {
-    @ResponseBody
     @RequestMapping("/")
     public String index() {
         return new Date().toString();
