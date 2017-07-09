d218460dddee2c072f30b7eba8d82557f0aa1a2e
Michael Lesniak <mlesniak@micromata.de>
2017-07-09 08:45:39 +0200

    Add model value

--- src/main/java/com/micromata/webengineering/demo/MainController.java
+++ src/main/java/com/micromata/webengineering/demo/MainController.java
@@ -1,6 +1,7 @@
 package com.micromata.webengineering.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Date;
@@ -8,8 +9,9 @@ import java.util.Date;
 @Controller
 public class MainController {
     @RequestMapping("/")
-    public String index() {
-        return new Date().toString();
+    public String index(Model model) {
+        model.addAttribute("date", new Date());
+        return "index";
     }
 }
 
