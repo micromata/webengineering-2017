a5bdd4fcd18c71b8a50d332bbb39a2545d7bd3b7
Michael Lesniak <mlesniak@micromata.de>
2017-07-09 07:18:02 +0200

    Add base page with header and footer

---
+++ src/main/java/com/micromata/webengineering/demo/BasePage.java
@@ -0,0 +1,10 @@
+package com.micromata.webengineering.demo;
+
+import org.apache.wicket.markup.html.WebPage;
+
+public class BasePage extends WebPage {
+    public BasePage() {
+        // For header and footer which consist only of markup we do not have
+        // to do anything here.
+    }
+}
