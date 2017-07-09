f370b181943ae0fd7a155d2431f746b0374d6eff
Michael Lesniak <mlesniak@micromata.de>
2017-07-09 06:31:48 +0200

    Generate random list elements

--- src/main/java/com/micromata/webengineering/demo/ExamplePage.java
+++ src/main/java/com/micromata/webengineering/demo/ExamplePage.java
@@ -2,5 +2,18 @@ package com.micromata.webengineering.demo;
 
 import org.apache.wicket.markup.html.WebPage;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.UUID;
+
 public class ExamplePage extends WebPage {
+    private List<String> list;
+
+    public ExamplePage() {
+        // Add 10 random elements.
+        list = new LinkedList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(UUID.randomUUID().toString());
+        }
+    }
 }
