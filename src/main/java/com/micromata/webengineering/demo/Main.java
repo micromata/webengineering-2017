f54f3d287b9d5d731328413a0fb9111d2ccd0694
Michael Lesniak <mlesniak@micromata.de>
2017-07-09 05:20:58 +0200

    Reset project

--- src/main/java/com/micromata/webengineering/demo/Main.java
+++ src/main/java/com/micromata/webengineering/demo/Main.java
@@ -1,40 +1,14 @@
 package com.micromata.webengineering.demo;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Main entry point and configuration base of the application.
  */
-@EnableSwagger2
 @SpringBootApplication
 public class Main {
-    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
-
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-    }
-
-
-    /**
-     * Configuration for Swagger.
-     *
-     * @return a docket.
-     */
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
     }
 }
--- src/main/java/com/micromata/webengineering/demo/comment/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.micromata.webengineering.demo.comment;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CommentRepository extends CrudRepository<Comment, Long> {
-    // Empty.
-}
