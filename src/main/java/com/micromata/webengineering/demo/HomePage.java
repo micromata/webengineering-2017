600c3462f8006d3e460f013cf852a67ff1936519
Michael Lesniak <mlesniak@micromata.de>
2017-07-09 05:55:11 +0200

    Add HomePage

---
+++ src/main/java/com/micromata/webengineering/demo/HomePage.java
@@ -0,0 +1,13 @@
+package com.micromata.webengineering.demo;
+
+import com.giffing.wicket.spring.boot.context.scan.WicketHomePage;
+import org.apache.wicket.markup.html.WebPage;
+
+/**
+ * Default homepage for Wicket.
+ */
+@WicketHomePage
+public class HomePage extends WebPage {
+    public HomePage() {
+    }
+}
