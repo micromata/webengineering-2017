188b0cf45e281784753608da53d5891e3bd42201
Michael Lesniak <mlesniak@micromata.de>
2017-07-09 09:25:10 +0200

    Rename default message

--- src/main/java/com/micromata/webengineering/demo/MainController.java
+++ src/main/java/com/micromata/webengineering/demo/MainController.java
@@ -6,13 +6,11 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.Date;
-
 @Controller
 public class MainController {
     @RequestMapping("/")
     public String index(Model model) {
-        model.addAttribute("value", new Date());
+        model.addAttribute("value", "some message");
         return "index";
     }
 
