f66d213c6ed2ba0dbed9387653665f60ddc6c99a
Michael Lesniak <mlesniak@micromata.de>
2017-07-09 06:07:29 +0200

    Use a model to show the variable's content

--- src/main/java/com/micromata/webengineering/demo/HomePage.java
+++ src/main/java/com/micromata/webengineering/demo/HomePage.java
@@ -3,6 +3,7 @@ package com.micromata.webengineering.demo;
 import com.giffing.wicket.spring.boot.context.scan.WicketHomePage;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.PropertyModel;
 
 /**
  * Default homepage for Wicket.
@@ -12,6 +13,7 @@ public class HomePage extends WebPage {
     private String message = "Hello world (variable)";
 
     public HomePage() {
-        add(new Label("message", message));
+        PropertyModel<String> messageModel = new PropertyModel<>(this, "message");
+        add(new Label("message", messageModel));
     }
 }
