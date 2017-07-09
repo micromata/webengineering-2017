c6b840fa69d7210c43ea694497b4b5a960336d69
Michael Lesniak <mlesniak@micromata.de>
2017-07-09 08:11:09 +0200

    Deconstruct REST-based annotation

--- src/main/java/com/micromata/webengineering/demo/MainController.java
+++ src/main/java/com/micromata/webengineering/demo/MainController.java
@@ -1,12 +1,14 @@
 package com.micromata.webengineering.demo;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Date;
 
-@RestController
+@Controller
 public class MainController {
+    @ResponseBody
     @RequestMapping("/")
     public String index() {
         return new Date().toString();
