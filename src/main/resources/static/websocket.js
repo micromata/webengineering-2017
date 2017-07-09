4ad39c9db398effdc5187e45335cf66ba78b9d92
Michael Lesniak <mlesniak@micromata.de>
2017-07-09 10:22:16 +0200

    Add simple JS code to show messages from server

---
+++ src/main/resources/static/websocket.js
@@ -0,0 +1,24 @@
+// Websocket example.
+// Since we are using no babel, etc... no ES6 features can be used.
+
+// See https://de.wikipedia.org/wiki/WebSocket
+
+var url = "<to be defined>";
+var socket = new WebSocket(url);
+
+socket.onopen = function () {
+    console.log("Connection established...");
+};
+
+
+socket.onmessage = function (messageEvent) {
+    console.log(messageEvent.data);
+};
+
+socket.onerror = function (errorEvent) {
+    console.log("Error. Connection closed.");
+};
+
+socket.onclose = function (closeEvent) {
+    console.log('Connection closed. Code: ' + closeEvent.code + '; Reason: ' + closeEvent.reason);
+};
